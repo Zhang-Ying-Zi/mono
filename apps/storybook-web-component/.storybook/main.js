5fe46c5b3fb59fbf1873f3debc0cf96651c63e06
Molly2Molly <489573239@qq.com>
2022-06-28 10:19:17 +0800

    combine storybook

--- apps/storybook-web-component/.storybook/main.js
+++ apps/storybook-web-component/.storybook/main.js
@@ -1,14 +1,22 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/web-components",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  refs: {
+    react: {
+      title: "React",
+      url: "http://localhost:6007",
+    },
+    angular: {
+      title: "Angular",
+      url: "http://localhost:6008",
+    },
+    vue: {
+      title: "Vue",
+      url: "http://localhost:6009",
+    },
+  },
+};
