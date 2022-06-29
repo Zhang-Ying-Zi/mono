a1dd5b638fec1290bec3947ca554d75d8ca499a1
Molly2Molly <489573239@qq.com>
2022-06-29 11:06:59 +0800

    update

--- apps/storybook-web-component/.storybook/preview.js
+++ apps/storybook-web-component/.storybook/preview.js
@@ -1,3 +1,9 @@
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import scss from "react-syntax-highlighter/dist/esm/languages/prism/scss";
+
+// Registers and enables scss language support
+SyntaxHighlighter.registerLanguage("scss", scss);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
